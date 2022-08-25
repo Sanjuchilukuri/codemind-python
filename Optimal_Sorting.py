d270eb3db5486c14ec864ec4269dcbec2d9ec2b7
sanju <81815695+Sanjuchilukuri@users.noreply.github.com>
2022-08-25 22:37:04 +0530

    t=int(input()) while(t):     a=int(input())     s=list(map(int,input().split()))     b=sorted(s)     if(s==b):         print(0)     else:         print(max(b)-min(b))     a-=1

---
+++ Optimal_Sorting.py
@@ -0,0 +1,10 @@
+t=int(input())
+while(t):
+    a=int(input())
+    s=list(map(int,input().split()))
+    b=sorted(s)
+    if(s==b):
+        print(0)
+    else:
+        print(max(b)-min(b))
+    a-=1
