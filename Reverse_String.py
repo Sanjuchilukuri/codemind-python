bfcf97746442afbf794e05f0206366e41e801ba2
sanju <81815695+Sanjuchilukuri@users.noreply.github.com>
2022-08-25 22:22:41 +0530

    l=list(map(str,input().split())) l1=l s1="" l1.reverse() for i in range(len(l1)):     if i==0:         s1+=l1[i]     else:         s1+=" "+l1[i] print(s1)

--- Reverse_String.py
+++ Reverse_String.py
@@ -1,2 +1,10 @@
-s=input()
-print(s[::-1])
+l=list(map(str,input().split()))
+l1=l
+s1=""
+l1.reverse()
+for i in range(len(l1)):
+    if i==0:
+        s1+=l1[i]
+    else:
+        s1+=" "+l1[i]
+print(s1)
