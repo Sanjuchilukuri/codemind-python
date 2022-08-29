a03379173016f563cf5c87cd06b0191ad2743780
sanju <81815695+Sanjuchilukuri@users.noreply.github.com>
2022-08-29 16:15:14 +0530

    from math import * def isprime(n):     if(n==1):         return False     sq=int(sqrt(n))     for i in range(2,sq+1):         if(n%i==0):             return False     return True
    n=int(input())
    b=0
    a=0
    for i in range(n,1,-1):
        if(isprime(i)):
            b+=i
            break
    for i in range(n,99999):
        if(isprime(i)):
            a+=i
            break
    dif=n-b
    diff=a-n
    if(dif>diff):
        print(diff)
    else:
        print(dif)

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,29 @@
+from math import *
+def isprime(n):
+    if(n==1):
+        return False
+    sq=int(sqrt(n))
+    for i in range(2,sq+1):
+        if(n%i==0):
+            return False
+    return True
+
+
+
+n=int(input())
+b=0
+a=0
+for i in range(n,1,-1):
+    if(isprime(i)):
+        b+=i
+        break
+for i in range(n,99999):
+    if(isprime(i)):
+        a+=i
+        break
+dif=n-b
+diff=a-n
+if(dif>diff):
+    print(diff)
+else:
+    print(dif)
