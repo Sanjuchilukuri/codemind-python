77377c6de359ab2c1bc8b70d6bdd29ecaf038e88
sanju <81815695+Sanjuchilukuri@users.noreply.github.com>
2022-09-24 11:00:13 +0530

    n=int(input()) arr=list(map(int,input().split())) l=[] #k=[] Sum=0 a,b=map(int,input().split()) for i in range(a,b+1):     l.append(i) for i in arr:     if  i  in l:         #k.append(i)         Sum=Sum+i print(Sum)

---
+++ sum_of_elements_between_A_and_B.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().split()))
+l=[]
+#k=[]
+Sum=0
+a,b=map(int,input().split())
+for i in range(a,b+1):
+    l.append(i)
+for i in arr:
+    if  i  in l:
+        #k.append(i)
+        Sum=Sum+i
+print(Sum)
