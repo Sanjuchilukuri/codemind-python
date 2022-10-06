51ea2a01520608efa91d62c2d8ab5da5e5ed26f3
sanju <81815695+Sanjuchilukuri@users.noreply.github.com>
2022-10-06 11:26:43 +0530

    from math import * def isprime(n):     if n==1:         return False     sq=int(sqrt(n))     for i in range(2,sq+1):         if n%i==0:             return False     return True
    c=0
    n=int(input())
    l=list(map(int,input().split()))
    k=int(input())
    for i in l:
        if i<=k:
            if isprime(i):
                c+=1
    print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,19 @@
+from math import *
+def isprime(n):
+    if n==1:
+        return False
+    sq=int(sqrt(n))
+    for i in range(2,sq+1):
+        if n%i==0:
+            return False
+    return True
+
+c=0
+n=int(input())
+l=list(map(int,input().split()))
+k=int(input())
+for i in l:
+    if i<=k:
+        if isprime(i):
+            c+=1
+print(c)
